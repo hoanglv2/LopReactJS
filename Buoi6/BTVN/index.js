db292ab668198edfcfe9229ffc7354e704563ef4
HoangLV2 <hoanglevanpro@gmail.com>
2021-04-20 06:06:31 +0700

    Nop BTVN buoi 6

---
+++ Buoi6/BTVN/index.js
@@ -0,0 +1,35 @@
+var a = prompt("Nhap so a:")
+var b = prompt("Nhap so b:")
+var c = prompt("Nhap so c:")
+var delta = b*b-(4*a*c)
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert("Hay nhap vao so!")
+}
+else if(a == 0) {
+    if(b == 0){
+        if(c == 0){
+            alert("Phương trình có vô số nghiệm")
+        }
+        else{
+            alert("Phương trình vô nghiệm")
+        }
+    }else {
+        alert("Phương trình có nghiệm duy nhất x = "+(-c/b))
+        }
+}
+else {
+    if(delta > 0) {
+        var x1 = (-b+Math.sqrt(delta))/(2*a);
+        var x2 = (-b-Math.sqrt(delta))/(2*a);
+        alert('Nghiệm thứ nhất x1 = '+x1+'\n Nghiệm thứ 2 x2 = '+x2)
+    } else if ( delta == 0) {
+        var sum = -b/2*a
+        alert('Phương trình có nghiệm kép: x1 = x2 = '+sum)
+    } else {
+        alert('Phương trình vô nghiệm')
+    }
+}
+    
+
+    
---
+++ Buoi6/index.js
@@ -0,0 +1,45 @@
+console.log("Hello World 12121212")
+// var a = 10;
+// var b = 'day "la" chuoi 1'
+// var c = "day la chuoi 2"
+// var d = `day la ${b} 12344 ${c} chuoi 3 ${a}`
+// console.log(a)
+// console.log(typeof a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(typeof d)
+// var testBoolF = false
+// var testBoolT= true
+// var car = {
+//     color: 'red',
+//     price: 2000,
+//     isBuy: true
+// }
+// console.log( car)
+// console.log(car.price)
+// var array = [1,2,'xi chao', car, 3,4,5]
+// console.log(array)
+// console.log(array[3])
+// var undefA;
+// console.log(typeof undefA)
+// var scopeA = 10
+// console.log(scopeA)
+
+// Các phép toán trong JS
+let congA = 10
+let congB = 20
+console.log(congA + congB)
+let soSanhA = 10;
+let soSanhB = '10';
+// Các phép toán so sánh: ==, ===, !=, !==
+// So sánh == ko so sánh đến kiểu dữ liệu, chỉ so sánh về giá trị
+// So sánh === so sánh cả kiểu dữ liệu và giá trị
+// Câu điều kiện
+if (soSanhA === soSanhB){
+    console.log('2 thang nay bang nhau')
+}
+else{
+    console.log('2 thang nay khac nhau');
+}
+// BTVN: giải phương trình bậc 2
